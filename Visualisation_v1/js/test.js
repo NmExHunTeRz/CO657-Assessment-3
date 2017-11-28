a3e3b272691a45367c86245bf94189baec082154
Huy Nguyen <kila.huy@gmail.com>
2017-11-28 11:58:12 +0000

    Add test for GET devices and device

--- Visualisation_v1/js/test.js
+++ Visualisation_v1/js/test.js
@@ -34,20 +34,55 @@ function getSites() {
 
 /* 
 	GET devices
+	Returns 35 devices sorted by sensor type
  */
-function getDevices() {
+var devices = [];
+function getDevicesID() {
 	url = "http://shed.kent.ac.uk/devices";
 	$.get(url, function(data) {
-
+		var table = "<table><thead><tr><th>Type</th><th>ID</th></tr></thead>"
+				  + "<tbody>";
+		$.each(data, function(index, item) {
+			var key = index;
+			var rows = item.length + 1;
+			table += "<tr><td rowspan='" + rows + "'>" + key + "</td>"
+			$.each(item, function(itemindex, id) {
+				table += "<tr><td>" + id + "</td></tr>";
+				devices.push(id);
+			});
+			table += "</tr>"
+		});
+		table += "</tbody></table>";
+		$('#devices_id').html(table);
+		getDevicesInfo();
 	});
 }
 
 /* 
 	GET device
+	Populate table with results for each device
  */
-function getDevice() {
-	url = "http://shed.kent.ac.uk/devices";
-	$.get(url, function(data) {
-
+function getDevicesInfo() {
+	url = "http://shed.kent.ac.uk/device/";
+	var table = "<table><thead><tr><th>ID</th><th>Name</th><th>Site</th><th>Zone</th><th>Last Connection</th><th>Software Version</th></tr></thead>"
+			  + "<tbody>";
+	$.each(devices, function(index, item) {
+		var query = url + item;
+		$.get(query, function(data) {
+			table += "<tr>"
+			      +  "<td>" + data.id + "</td>"
+			      +  "<td>" + data.name + "</td>"
+			      +  "<td>" + data.site_id + "</td>"
+			      +  "<td>" + data.zone_id + "</td>"
+			      +  "<td>" + data.last_connection + "</td>"
+			      +  "<td>" + data.software_version + "</td>"
+			      +  "</tr>";
+			console.log(table);
+			console.log("");
+			if (index == devices.length - 1) {
+				table += "</tbody></table>";
+				$('#devices').html(table);
+			}
+		});
 	});
 }
